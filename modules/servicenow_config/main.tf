ebde67241067bad822b05ca0edba11af2ff383fa
Mustafa M <neyaadeez@gmail.com>
2025-10-16 18:04:35 -0400

    resolved password encoding decoding issue

--- modules/servicenow_config/main.tf
+++ modules/servicenow_config/main.tf
@@ -152,6 +152,9 @@ resource "null_resource" "configure_servicenow" {
       
       echo "Using cached session cookies from: $cookie_file"
       
+      # URL encode the password to handle special characters like %
+      encoded_password=$(printf '%s' "${var.servicenow_config.password}" | sed 's/%/%25/g; s/ /%20/g; s/!/%21/g; s/"/%22/g; s/#/%23/g; s/\$/%24/g; s/&/%26/g; s/'\''/%27/g; s/(/%28/g; s/)/%29/g; s/\*/%2A/g; s/+/%2B/g; s/,/%2C/g; s/-/%2D/g; s/\./%2E/g; s/\//%2F/g; s/:/%3A/g; s/;/%3B/g; s/</%3C/g; s/=/%3D/g; s/>/%3E/g; s/?/%3F/g; s/@/%40/g; s/\[/%5B/g; s/\\/%5C/g; s/\]/%5D/g; s/\^/%5E/g; s/_/%5F/g; s/`/%60/g; s/{/%7B/g; s/|/%7C/g; s/}/%7D/g; s/~/%7E/g')
+      
       # Make API call to configure ServiceNow using both shared session cookies and auth token
       response=$(curl -s -w "\nHTTP_STATUS:%%{http_code}" \
         -X POST \
@@ -163,7 +166,7 @@ resource "null_resource" "configure_servicenow" {
         -d "service_host=${var.servicenow_config.service_host}" \
         -d "proxy=${var.servicenow_config.proxy}" \
         -d "account=${var.servicenow_config.account}" \
-        -d "password=${var.servicenow_config.password}" \
+        -d "password=$encoded_password" \
         -d "dampeningPeriod=${var.servicenow_config.dampening_period}" \
         -d "appId=${var.servicenow_config.app_id}" \
         -d "appKey=${var.servicenow_config.app_key}" \
@@ -207,7 +210,7 @@ resource "null_resource" "configure_servicenow" {
             -d "service_host=${var.servicenow_config.service_host}" \
             -d "proxy=${var.servicenow_config.proxy}" \
             -d "account=${var.servicenow_config.account}" \
-            -d "password=${var.servicenow_config.password}" \
+            -d "password=$encoded_password" \
             -d "dampeningPeriod=${var.servicenow_config.dampening_period}" \
             -d "appId=${var.servicenow_config.app_id}" \
             -d "appKey=${var.servicenow_config.app_key}" \
